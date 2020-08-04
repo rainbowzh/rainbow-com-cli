9620cb0aab09800bd8a9beb35eaff2bddfc8ab29
zhouhong07 <zhouhong07@58.com>
2020-08-04 16:18:16 +0800

    添加移动端适配和pxtorem

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 #### 关于
-##### 支持哪些新方式 ：
+##### 支持哪些新特性 ：
 - tree shaking
 - build after gzip
-- corejs3 pofilly
+- corejs3 polyfill
 - image limit output
 - support scss
+---
+##### 使用方式：
+* 加载配置
+```
+  cnpm install 
+```
+* 开启本地环境
+```
+  cnpm run start
+```
+* 生成生产环境
+```
+  cnpm run build
+```
--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,14 @@
  * @Author: zhouhong07
  * @Date: 2020-08-04 14:35:16
  * @LastEditors: zhouhong07
- * @LastEditTime: 2020-08-04 15:10:02
+ * @LastEditTime: 2020-08-04 16:07:49
  */ 
 module.exports = {
   plugins: [
-    require('autoprefixer')({ overrideBrowserslist: ['last 5 version', '>1%', 'ie >=8'] })
+    require('autoprefixer')({ overrideBrowserslist: ['last 5 version', '>1%', 'ie >=8'] }) ,
+    require('postcss-pxtorem')({
+      rootValue: 75,//结果为：设计稿元素尺寸/16，比如元素宽320px,最终页面会换算成 20rem
+      propList: ['*']
+    })
   ]
 };
